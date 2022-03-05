e505c05c2b9b52724df95decf9d9e7c1a5888609
jendelcas <jendelcas83@gmail.com>
2022-03-05 19:13:51 -0300

    Error de conneccion

--- lib/db.php
+++ lib/db.php
@@ -5,6 +5,7 @@ require_once 'php-activerecord/ActiveRecord.php';
 activerecord\config::initialize(function ($cfg) {
     $cfg->set_model_directory('models');
     $cfg->set_connections(array(
-        'production' => 'mysql://b5979641148518:cacc6979@us-cdbr-east-05.cleardb.net/heroku_5adbccd2b7cd39b?reconnect=true'
+        //'development' => 'mysql://root:@localhost/farm_db',
+        'development' => 'mysql://b5979641148518:cacc6979@us-cdbr-east-05.cleardb.net/heroku_5adbccd2b7cd39b?reconnect=true'
     ));
 });
