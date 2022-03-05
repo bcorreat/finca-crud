86f0867a1dab9a188d1bdf12460fe8ec3d8aa131
jendelcas <jendelcas83@gmail.com>
2022-03-05 19:02:29 -0300

    Modificación para ajustar error en BD

--- lib/db.php
+++ lib/db.php
@@ -5,7 +5,6 @@ require_once 'php-activerecord/ActiveRecord.php';
 activerecord\config::initialize(function ($cfg) {
     $cfg->set_model_directory('models');
     $cfg->set_connections(array(
-        'development' => 'mysql://root:@localhost/farm_db',
         'production' => 'mysql://b5979641148518:cacc6979@us-cdbr-east-05.cleardb.net/heroku_5adbccd2b7cd39b?reconnect=true'
     ));
 });
