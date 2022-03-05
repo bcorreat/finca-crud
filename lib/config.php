e94f716a5cbaaf418eeca3d02fe6a3ec912f2ad0
Jennyfer Del Castillo <jendelcas83@gmail.com>
2022-03-05 16:12:41 -0300

    Update config.php
    Conexion a base de datos en servidor

--- lib/config.php
+++ lib/config.php
@@ -6,6 +6,6 @@ activerecord\config::initialize(function ($cfg) {
     $cfg->set_model_directory('models');
     $cfg->set_connections(array(
         'development' => 'mysql://root:@localhost/farm_db',
-        'production' => 'mysql://uirisr12clmusp3o:K9G7TL1VTJfoOQ2dYN5I@bxeqtmxp82uhia8didlv-mysql.services.clever-cloud.com:3306/bxeqtmxp82uhia8didlv'
+        'production' => 'mysql://b5979641148518:cacc6979@us-cdbr-east-05.cleardb.net/heroku_5adbccd2b7cd39b?reconnect=true'
     ));
 });
